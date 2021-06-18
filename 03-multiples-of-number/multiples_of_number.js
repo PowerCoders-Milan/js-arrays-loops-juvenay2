d1033640eded2f8aa6253bcf16d95da2cfa7288f
rinaldi <juvenay2@gmail.com>
2021-06-18 18:07:03 +0200

    done

--- 03-multiples-of-number/multiples_of_number.js
+++ 03-multiples-of-number/multiples_of_number.js
@@ -1,11 +1,15 @@
+
 const multiplesOf = (number,limit) => {
-      for (i=0; i<=10; i++){
-          if (i%2 === 0){
-              console.log(i);
-
+   const multiples = [];
+      for ( i=0; i<limit; i++){
+          if (i%number === 0){
+              multiples.push(i);
           }
+         
       }
-}
-multiplesOf();
+      return multiples;
+}     
 
-const multiples = [0,2,4,6,8,10];
+const multiplesOfTwo = multiplesOf(3,9);
+
+console.log(multiplesOfTwo);
