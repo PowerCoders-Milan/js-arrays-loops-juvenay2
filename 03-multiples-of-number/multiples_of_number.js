ebd9ae7ccfac9c19b39a2b99cb1ec07cd15969dd
rinaldi <juvenay2@gmail.com>
2021-06-18 14:33:09 +0200

    completed

--- 03-multiples-of-number/multiples_of_number.js
+++ 03-multiples-of-number/multiples_of_number.js
@@ -1,1 +1,11 @@
-// define your function here
+const multiplesOf = (number,limit) => {
+      for (i=0; i<=10; i++){
+          if (i%2 === 0){
+              console.log(i);
+
+          }
+      }
+}
+multiplesOf();
+
+const multiples = [0,2,4,6,8,10];
