6b1f03c05c96a80d16940b8487b77d2c2069b5f5
rinaldi <juvenay2@gmail.com>
2021-06-21 11:52:00 +0200

    done

--- 04-n-multiples-of-number/n_multiples.js
+++ 04-n-multiples-of-number/n_multiples.js
@@ -1,6 +1,27 @@
 // function declaration
 const nMultiplesOf = (number, limit) => {
+        multiplesOfNumber = [];
+      let a = multiplesOfNumber;
 
-}
+      let i = number;
+       while (i<limit){
+          if (i%number === 0) {
+            multiplesOfNumber.push(i);
+            multiplesOfNumber.length >= limit;   
+          }
+           i++;   
+       }
+    
+       return multiplesOfNumber
+    }
 
-console.log(nMultiplesOf(2, 10))
+console.log(nMultiplesOf(2, 20))
+
+
+
+
+// let counterTwo = 1;
+// while (counterTwo < 4) {
+//   console.log(counterTwo);
+//   counterTwo++;
+// }
